44cefc4e9b7160307703a5de679e1bb5e8a1a0f6
Patrick Barbosa <patrick.oliveira27@hotmail.com>
2025-03-26 19:13:20 -0300

    corrigindo deploy3

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ def webhook():
         return jsonify({"status": "success"}), 200
 
 if __name__ == '__main__':
-    app.run(port=os.getenv("PORT"), debug=True)
+    app.run(host='0.0.0.0', port=os.getenv("PORT"), debug=True)
